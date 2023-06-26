9e66306e8d75dc786c4270227897fa0a0d26836c
fika4life <fistewie@gmail.com>
2023-06-26 21:32:32 +0200

    better

--- src/lib/supabase/supabaseClient.js
+++ src/lib/supabase/supabaseClient.js
@@ -1,6 +1,10 @@
+import {
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_ANON_KEY
+} from '$env/static/public';
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://hyilybvbakkhdxeexsrg.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5aWx5YnZiYWtraGR4ZWV4c3JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcxOTk5OTcsImV4cCI6MjAwMjc3NTk5N30.YXodLjsNJyUzDWjDW7IDAcy-IOskikUvRPvbQsDTQDo'
+  PUBLIC_SUPABASE_URL,
+  PUBLIC_SUPABASE_ANON_KEY
 );
